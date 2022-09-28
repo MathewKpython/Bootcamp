cba0aec393fd01adce4582c492a3d58c08f76d35
Mateusz Kulik <kulio1991@gmail.com>
2022-09-28 06:02:07 +0200

    palindrome

---
+++ Bootcamp 42.py
@@ -0,0 +1,16 @@
+
+def Palindrome(word):
+    word = word.lower()
+    length = len(word)
+    if length < 2:
+        return True
+    elif word[0] == word[length - 1]:
+        return Palindrome(word[1: length - 1])
+    else:
+        return False
+word = "Kajak"
+answer = Palindrome(word)
+if answer:
+    print("Yes")
+else:
+    print("No")
